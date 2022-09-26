3c7efba9c9f04944d95a5f0488501189bdcf9592
JoachimJB <joachim@borresen.email>
2022-09-26 14:05:21 +0200

    Change text in scroll

--- DisplayLetters/src/main.adb
+++ DisplayLetters/src/main.adb
@@ -5,7 +5,7 @@ with MicroBit.DisplayRT; use MicroBit.DisplayRT;
 procedure Main is
 begin
    loop
-      Put_Line ("Serial: Hello Ravenscar!"); -- open a serial port to see this message
+      --Put_Line ("Serial: Hello Ravenscar!"); -- open a serial port to see this message
 
       Scroll_Text("Joachim"); --what happens if we have a longer text?
 
