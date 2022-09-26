eeb4e082f05cec3572f7020d9676449a3bf842f3
JoachimJB <joachim@borresen.email>
2022-09-26 19:12:56 +0200

    Added pins to all 4 motors

--- Motor/src/main.adb
+++ Motor/src/main.adb
@@ -4,14 +4,21 @@ with MicroBit.Time;
 
 procedure Main is
    --Motor 1
-   PIN_ENA : constant Pin_Id := 0;
+   PIN_EN1 : constant Pin_Id := 0;
    PIN_IN1 : constant Pin_Id := 1;
    PIN_IN2 : constant Pin_Id := 2;
    --Motor 2
-   PIN_ENB : constant Pin_Id := 0;
-   PIN_IN3 : constant Pin_Id := 1;
-   PIN_IN4 : constant Pin_Id := 2;
-
+   PIN_EN2 : constant Pin_Id := 4;
+   PIN_IN3 : constant Pin_Id := 5;
+   PIN_IN4 : constant Pin_Id := 6;
+   --Motor 3
+   PIN_EN3 : constant Pin_Id := 7;
+   PIN_IN5 : constant Pin_Id := 8;
+   PIN_IN6 : constant Pin_Id := 9;
+   --Motor 4
+   PIN_EN4 : constant Pin_Id := 10;
+   PIN_IN7 : constant Pin_Id := 11;
+   PIN_IN8 : constant Pin_Id := 12;
 begin
 
    loop
