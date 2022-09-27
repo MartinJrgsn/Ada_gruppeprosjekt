ce51a04b8f93fa0091b48d048ff2b27ee7331b1b
JoachimJB <joachim@borresen.email>
2022-09-27 12:49:01 +0200

    Correct pin selection and use of digital/analog write

--- DCMotor/src/main.adb
+++ DCMotor/src/main.adb
@@ -1,14 +1,32 @@
 with MicroBit.IOs; use MicroBit.IOs;
+with MicroBit.Time;
 
 procedure Main is
-   --Motor 1
+   --Motor 1 (FR)
    PIN_EN1 : constant Pin_Id := 0;
-   PIN_IN1 : constant Pin_Id := 1;
-   PIN_IN2 : constant Pin_Id := 2;
+   PIN_IN1 : constant Pin_Id := 11;
+   PIN_IN2 : constant Pin_Id := 12;
+   --Motor 2 (FL)
+   PIN_EN2 : constant Pin_Id := 1;
+   PIN_IN3 : constant Pin_Id := 13;
+   PIN_IN4 : constant Pin_Id := 14;
+
 begin
    loop
-      Write (PIN_EN1, 0);
-      Write (PIN_IN1, 0);
-      Write (PIN_IN2, 0);
+      --Initial direction
+      --Motor 1
+      Set (PIN_IN1, False);
+      Set (PIN_IN2, True);
+      --Motor 2
+      Set (PIN_IN3, False);
+      Set (PIN_IN4, True);
+
+
+      --For loop for testing purposes.
+      --(Fading LED to check wheter flash is successfull)
+      for Value in Analog_Value range 0 .. 256 loop
+         Write (10, Value);
+         MicroBit.Time.Delay_Ms(5);
+      end loop;
    end loop;
 end Main;
