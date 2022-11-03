b25bba6a52867550fba450a35d3c3d7e9520beea
Martin Jørgensen <mrtnjrgsn@gmail.com>
2022-11-03 17:54:58 +0100

    Setup based on tasking_project_sense_think_act project

--- Stage_One/src/Tasks/act.ads
+++ Stage_One/src/Tasks/act.ads
@@ -1,5 +1,11 @@
+with Motor_Driver; use Motor_Driver;
+
 package Act is
 
-   Task Act with Priority => 3; -- Highest priority will be to act
+   task Act with Priority => 3; -- Highest priority will be to act
+
+   procedure SetupMotors;
+   procedure Drive (direction : Directions);
+   procedure ControlMotor(Instruction : DriveInstruction; Pins : MotorControllerPins);
 
 end Act;
--- Stage_One/src/Tasks/sense.ads
+++ Stage_One/src/Tasks/sense.ads
@@ -1,5 +1,7 @@
+with Car_Brain; use Car_Brain;
+
 package Sense is
 
-   task Sense with Priority => 1;
+   task Sense with Priority => 1; --random priority. argue and calculate what these values should be
 
 end Sense;
