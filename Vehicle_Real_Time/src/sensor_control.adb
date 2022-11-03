a2ac174eb6f64ec9d64f67ca8841e717d9db0338
JoachimJB <joachim@borresen.email>
2022-11-03 14:04:32 +0100

    updates

--- Vehicle_Real_Time/src/sensor_control.adb
+++ Vehicle_Real_Time/src/sensor_control.adb
@@ -7,38 +7,40 @@ package body Sensor_Control is
 
    Task body Sensor_Read is
       Time_Now : Ada.Real_Time.Time;
-
+      delay_counter : Integer := 100;
+      delay_interval : Integer := 100;
       
    begin
       loop
          Time_Now := Ada.Real_Time.Clock;
-
+         
+         
          Go(10, 0);
-         delay until Time_now + Ada.Real_Time.Milliseconds(200);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 45);
-         delay until Time_now + Ada.Real_Time.Milliseconds(400);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 90);
-         delay until Time_now + Ada.Real_Time.Milliseconds(600);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 135);
-         delay until Time_now + Ada.Real_Time.Milliseconds(800);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 180);
-         delay until Time_now + Ada.Real_Time.Milliseconds(1000);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 135);
-         delay until Time_now + Ada.Real_Time.Milliseconds(1200);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 90);
-         delay until Time_now + Ada.Real_Time.Milliseconds(1400);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          Go(10, 45);
-         delay until Time_now + Ada.Real_Time.Milliseconds(1600);
+         delay until Time_now + Ada.Real_Time.Milliseconds(delay_counter);
+         delay_counter := delay_counter + delay_interval;
          
-         
-         
-         
-         
-         
-         
-         
---         Put_line("Sensor_Read");
-  --       delay until Time_now + Ada.Real_Time.Seconds(1);
-      end loop;
+         delay_counter := delay_interval;
+         end loop;
    end Sensor_Read;
 end Sensor_Control;
